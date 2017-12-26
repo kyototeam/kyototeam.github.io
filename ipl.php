9003eefcdd2182963b31cfa941bb80edca548505
Hazro <hazrrey@gmail.com>
2017-12-26 17:00:48 +0100

    iwi

---
+++ ipl.php
@@ -0,0 +1,13 @@
+<?php
+// Do make a visitors.html file and set permission to 0777
+ 
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$dateTime = date('Y/m/d G:i:s');
+$file = "ips.txt";
+$file = fopen($file, "a");
+$data = "<pre><b>User IP</b>: $ip <b> Browser</b>: $browser <br>on Time : $dateTime <br></pre>";
+fwrite($file, $data);
+fclose($file);
+echo "<h1>Hi n0.0b :)</h1>";
+?>
